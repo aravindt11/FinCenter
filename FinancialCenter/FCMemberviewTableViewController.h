abf9323b204e1a05f63349e1ff381724dc477d0b
aravindt11 <whistlester@Aravinds-MacBook-Pro.local>
2014-12-30 20:00:42 -0600

    added user login

---
+++ FinancialCenter/FCMemberviewTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FCMemberview.h
+//  FinancialCenter
+//
+//  Created by Aravind Thiyagarajan on 12/30/14.
+//  Copyright (c) 2014 USAA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FCMemberviewTableViewController : UITableViewController
+
+@end
--- FinancialCenter/Login/FCLoginViewController.m
+++ FinancialCenter/Login/FCLoginViewController.m
@@ -14,6 +14,7 @@
 @end
 
 @implementation FCLoginViewController
+@synthesize userId,password;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -37,5 +38,82 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+
+//Validate username and password based on client requirment
+
+
+-(Boolean)validateEmail:(NSString *)userid password:(NSString *)password
+{
+    return true;
+    
+}
+
+//Login with userid and password need to be implemented
+
+-(Boolean)logInWithUsername:(NSString *)userid password:(NSString *)password
+{
+    return true;
+    
+}
+
+- (IBAction)login:(id)sender {
+    
+    NSString *username=self.userId.text;
+    BOOL flag=true;
+    
+    NSString *passwordGot=self.password.text ;
+    
+
+    
+    if (username && passwordGot && username.length != 0 && passwordGot.length != 0) {
+        //Check username and password validation
+        if ([self validateEmail:username password:passwordGot]) {
+            
+            
+        }
+        else
+        {
+            flag=false;
+            [[[UIAlertView alloc] initWithTitle:@"User name and passowrd Not Valid"
+                                        message:@"Please Enter details!"
+                                       delegate:nil
+                              cancelButtonTitle:@"ok"
+                              otherButtonTitles:nil] show];
+        }
+        
+        // Begin login process
+    }
+    else
+    {
+        flag=false;
+        [[[UIAlertView alloc] initWithTitle:@"Missing Information"
+                                    message:@"Make sure you fill out all of the information!"
+                                   delegate:nil
+                          cancelButtonTitle:@"ok"
+                          otherButtonTitles:nil] show];
+        // Interrupt login process
+    }
+
+    if([self logInWithUsername:username password:passwordGot])
+        
+    {
+        if([username isEqualToString:@"member"])
+        {
+            
+        }
+        else if ([username isEqualToString:@"wealthmanager"])
+        {
+            
+        }
+        else{
+            //floor manger
+        }
+        
+    }
+}
+
+
+
 
 @end
